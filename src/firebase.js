635666b716a5c82e3c6913da748ad42f03317ebb
Jafet San <j.s.b_95@outlook.com>
2025-05-28 20:07:57 -0600

    Configuración de Quasar y Firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRNEwIA09lZeaKlAPavdKDMReJ1XmXS6g",
+  authDomain: "cine-info-f17d9.firebaseapp.com",
+  projectId: "cine-info-f17d9",
+  storageBucket: "cine-info-f17d9.firebasestorage.app",
+  messagingSenderId: "704735667281",
+  appId: "1:704735667281:web:a86ed96cfc40fc6f344470"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Quasar } from 'quasar'  // Importa Quasar
+
+import firebaseApp from "./firebase";
+console.log("Firebase conectado:", firebaseApp);
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Quasar, { plugins: {} })  // Activa Quasar
 
 app.mount('#app')
